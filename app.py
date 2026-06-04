2ae1135a0316ad5c8f3edb690db23eb43dedb86c
Emanuel Lopez Higuita <EmLoH@users.noreply.huggingface.co>
2026-06-04 03:08:10 +0000

    Cambio de modelo

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ final_answer = FinalAnswerTool()
 model = HfApiModel(
 max_tokens=2096,
 temperature=0.5,
-model_id='Meta-Llama-3-8B-Instruct',
+model_id="Qwen/Qwen2.5-Coder-32B-Instruct",
 custom_role_conversions=None,
 )
 
